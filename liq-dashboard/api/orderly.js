3cf9189199a133756e9123df8429f981c7c39567
MonacoSize <alec@seinetwork.io>
2026-02-24 16:34:58 -0600

    Update orderly.js

--- liq-dashboard/api/orderly.js
+++ liq-dashboard/api/orderly.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
     const timeout = setTimeout(() => controller.abort(), 8000);
 
     const response = await fetch(url, {
+      method: 'GET',
       headers: { 'Accept': 'application/json' },
       signal: controller.signal,
     });
